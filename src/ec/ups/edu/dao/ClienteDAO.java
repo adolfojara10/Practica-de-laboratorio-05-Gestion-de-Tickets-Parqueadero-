6e4ff2d86d3b2c401acfabe05c07c7c869dd3260
JHON FAREZ <JHON FAREZ@DESKTOP-I45IDQE>
2020-06-22 23:21:27 -0500

    Creacion de la clase ClienteDAO, donde se implementa el CRUD Y listar usuarion

--- src/ec/ups/edu/dao/ClienteDAO.java
+++ src/ec/ups/edu/dao/ClienteDAO.java
@@ -5,10 +5,47 @@
  */
 package ec.ups.edu.dao;
 
+import ec.ups.edu.idao.IClienteDAO;
+import ec.ups.edu.modelo.Cliente;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author Adolfo
  */
-public class ClienteDAO {
+public class ClienteDAO implements IClienteDAO{
+
+    private Set<Cliente> clientes;
+    public ClienteDAO (){
+    clientes = new HashSet<Cliente>();
+    }
+    @Override
+    public void create(Cliente cliente) {
+    clientes.add(cliente);
+    }
+
+    @Override
+    public Cliente read(String cedula) {
+   return null;
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+
+    }
+
+    @Override
+    public void delete(Cliente cliente) {
+
+    }
+
+    @Override
+    public Set<Cliente> findAll() {
+    return clientes;
+    }
+    
+    
     
 }
